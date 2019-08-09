a376d7323de6a684c25e993be5d1a0f1ff6f523d
tsiry <YOUR@EMAIL.com>
2019-08-09 18:01:10 +0300

    type 3

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,13 +1,30 @@
 class GossipsController < ApplicationController
     
+    def new
+        @gossip = Gossip.new
+    end
+    
+    def create
+        gossip = Gossip.create(gossip_params)
+            redirect_to static_pages_index_path
+    end
+
     def edit
         @gossip = Gossip.find(params[:id])
     end 
 
+    def show
+        @gossip = Gossip.find(params[:id])
+    end
+
     def update
         @gossip = Gossip.find(params[:id])
-        gossip_params = params.require(:gossip).permit(:title, :content).inspect
         @gossip.update(gossip_params)
-            redirect_to index_path
+            redirect_to static_pages_index_path
     end
+
+    private
+        def gossip_params
+            params.require(:gossip).permit(:title, :content)
+        end
 end
