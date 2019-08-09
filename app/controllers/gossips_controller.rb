36c6f41397045e69470d48fd7852b868eed63442
tsiry <YOUR@EMAIL.com>
2019-08-09 14:58:28 +0300

    update

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,2 +1,13 @@
 class GossipsController < ApplicationController
+    
+    def edit
+        @gossip = Gossip.find(params[:id])
+    end 
+
+    def update
+        @gossip = Gossip.find(params[:id])
+        gossip_params = params.require(:gossip).permit(:title, :content).inspect
+        @gossip.update(gossip_params)
+            redirect_to index_path
+    end
 end
